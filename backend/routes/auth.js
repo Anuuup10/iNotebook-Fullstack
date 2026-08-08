ba7a3ffbb7a2efa84802a6a543717556214d3090
Anup Chaudhary <74030059+Anup10430@users.noreply.github.com>
2026-08-08 08:21:19 +0545

    Feat: Login Endpoint and sending auth token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require ("bcryptjs");
 const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = "Anup$isagoodboy";
-// Create a user using: POST "/api/auth/createuser". No login required
+// Authenticate a user using: POST "/api/auth/createuser". No login required
 router.post(
   "/createuser",
   [
@@ -50,9 +50,49 @@ router.post(
 
     } catch (error) {
       console.log(error.message);
-      res.status(500).send("Some Error occured");
+      res.status(500).send("Internal Server Error");
     }
   },
 );
 
+// Create a user using: POST "/api/auth/login". No login required
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+      // If there are errors, return bad request and the errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const {email, password} = req.body;
+      try {
+        let user = await User.findOne({email});
+        if(!user) {
+          return res.status(400).json({error: "Please try to login with correct credentials"});
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password);
+        if (!passwordCompare) {
+          return res.status(400).json({
+            error: "Please try to login with correct credentials"
+          });
+        }
+
+        const data = {
+          user:{
+            id: user.id
+          },
+        };
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json(authtoken);
+      } catch (error) {
+          console.log(error.message);
+          res.status(500).send("Internal Server Error");
+      }
+  })
 module.exports = router;
