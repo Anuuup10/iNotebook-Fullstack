33bd324aecd2d73799e03b2d7f422f45a4dac8d2
Anup Chaudhary <74030059+Anup10430@users.noreply.github.com>
2026-08-30 07:33:09 +0545

    useLocation Hook in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import NoteState from './context/notes/NoteState';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
     <>
       <NoteState>
-      <BrowserRouter>
-      <Navbar/>
-      {/* Routes */}
-      <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route exact path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
-    </NoteState>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            {/* Routes */}
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </NoteState>
     </>
   );
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,22 +3,9 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "Anup",
-        "class": "10B"
-    }
-
-    const [state, setstate] = useState(s1);
-    const update = () => {
-        setTimeout(() => {
-           setstate({
-            "name": "Lord",
-            "class": "11B"
-           }) 
-        }, 1000);
-    }
+  
     return(
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )
