f2874a69f9771e2f40f8a9d74943d77a3ff3ef4e
Anup Chaudhary <74030059+Anup10430@users.noreply.github.com>
2026-09-03 19:41:40 +0545

    Feat: implement AddNote component and integrate note addition functionality

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -38,21 +38,38 @@ const NoteState = (props) => {
     "tag": "personal",
     "date": "2026-08-11T02:10:43.313Z",
     "__v": 0
-  },
-  {
-    "_id": "6a7a84a3b949e58b138f57e94",
-    "user": "6a7694919c4783ec577ae3ee",
-    "title": "My Title",
-    "description": "Please wake up early",
-    "tag": "personal",
-    "date": "2026-08-11T02:10:43.313Z",
-    "__v": 0
   }
 ]
 
 const [notes, setNotes] = useState(notesInitial)
+
+// Add a note
+const addNote = (title, description, tag) => {
+  // TODO: API Call
+  const note = {
+    "_id": "6a7a84a3b949e58b138f57e94",
+    "user": "6a7694919c4783ec577ae3ee",
+    "title": title,
+    "description": description,
+    "tag": tag,
+    "date": "2026-08-11T02:10:43.313Z",
+    "__v": 0
+  }
+  setNotes(notes.concat(note))
+}
+
+// Delete a note
+const deleteNote = () => {
+
+}
+
+// Edit a note
+const editNote = () => {
+
+}
+
     return(
-        <NoteContext.Provider value={{notes, setNotes}}>
+        <NoteContext.Provider value={{notes, setNotes, addNote, deleteNote, editNote}}>
             {props.children}
         </NoteContext.Provider>
     )
