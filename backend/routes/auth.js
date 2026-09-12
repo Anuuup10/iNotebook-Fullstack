0fa34988e1ad4092077b5328bb248223e8201ada
Anup Chaudhary <74030059+Anup10430@users.noreply.github.com>
2026-09-12 09:07:50 +0545

    Feat: Implement user authentication with Login component

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -63,6 +63,7 @@ router.post(
     body("password", "Password cannot be blank").exists(),
   ],
   async (req, res) => {
+    let success = false;
     // If there are errors, return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -73,6 +74,7 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (!user) {
+        success = false;
         return res
           .status(400)
           .json({ error: "Please try to login with correct credentials" });
@@ -80,8 +82,9 @@ router.post(
 
       const passwordCompare = await bcrypt.compare(password, user.password);
       if (!passwordCompare) {
+        success = false;
         return res.status(400).json({
-          error: "Please try to login with correct credentials",
+          success, error: "Please try to login with correct credentials",
         });
       }
 
@@ -91,7 +94,8 @@ router.post(
         },
       };
       const authtoken = jwt.sign(data, JWT_SECRET);
-      res.json(authtoken);
+      success = true;
+      res.json({success, authtoken});
     } catch (error) {
       console.log(error.message);
       res.status(500).send("Internal Server Error");
